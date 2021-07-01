3ecbb11b0316fcbcfb96ebeffa0a147aeb00d527
iakobi <iakobi.mirotadze@gmail.com>
2021-07-01 23:41:07 +0400

    V1.0.0

--- middleWares/constants.js
+++ middleWares/constants.js
@@ -2,8 +2,8 @@ const Config = {
     rates: {
         takeRate: 1,
         delivery: 2,
-        normalRate: 4.0,
-        expressRate: 6.0,
+        normalRate: 5.0,
+        expressRate: 7.0,
     }
 }
 module.exports = Config
