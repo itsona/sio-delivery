c4db1f98295b868443f5391289b81e3553dc25d8
Iakob Mirotadze <iakobi.mirotadze@gmail.com>
2024-02-23 20:16:26 +0400

    test

--- middleWares/authentication.js
+++ middleWares/authentication.js
@@ -4,34 +4,28 @@ const MongoClient = require('mongodb').MongoClient;
 const newUrl = `mongodb://127.0.0.1:27017/`;
 // const newUrl = `mongodb+srv://SioRoot:aFacitFJKjYX9RW2@siodelivery.gw9lbkf.mongodb.net/`;
 let db = null
-     MongoClient.connect(newUrl, {useUnifiedTopology: false}).then(database=> {db = database})
+     MongoClient.connect(newUrl, {useUnifiedTopology: true}).then(database=> {db = database})
 
 
 const userData = async ()=> {
     const dbo = await db.db('delivery');
-    setTimeout(()=> db.close(), 2000)
     return {res: dbo.collection("users"), db: {close: ()=> {}}};
 }
 const emailData = async ()=> {
     const dbo = await db.db('delivery');
-    setTimeout(()=> db.close(), 2000)
 
     return {res: dbo.collection("emails"), db: {close: ()=> {}}};
 }
 const paymentsData = async ()=> {
     const dbo = await db.db('delivery');
-    setTimeout(()=> db.close(), 2000)
-
     return {res: dbo.collection("payments"), db: {close: ()=> {}}};
 }
 const pageData = async ()=> {
     const dbo = await db.db('delivery');
-    setTimeout(()=> db.close(), 2000)
     return {res: dbo.collection("data"), db: {close: ()=> {}}};
 }
 const logData = async ()=> {
     const dbo = await db.db('delivery');
-    setTimeout(()=> db.close(), 2000)
     return {res: dbo.collection("log"), db: {close: ()=> {}}};
 }
 
